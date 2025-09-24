7c25a390953c1635e8fe45e75488f31084475448
Corentin Arnaud <corentin.arnaud45@gmail.com>
2025-09-24 14:12:51 +0200

    modification global variables
    horrible dev

--- main.py
+++ main.py
@@ -3,35 +3,14 @@ import matplotlib.colors as mcolors
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import random
+import copy
 
-matrix = []
-matrix_range = 10
+matrix_range = 100
 
-# colors = [
-#     # Greens (light → dark)
-#     [200/255, 255/255, 200/255, 1.0],
-#     [120/255, 200/255, 120/255, 1.0],
-#     [60/255, 140/255, 60/255, 1.0],
-#     [20/255, 80/255, 20/255, 1.0],
-
-#     # Blues (light → dark)
-#     [200/255, 200/255, 255/255, 1.0],
-#     [120/255, 120/255, 200/255, 1.0],
-#     [60/255, 60/255, 140/255, 1.0],
-#     [20/255, 20/255, 80/255, 1.0],
-
-#     # Yellows (light → dark)
-#     [255/255, 255/255, 200/255, 1.0],
-#     [220/255, 220/255, 120/255, 1.0],
-#     [180/255, 180/255, 60/255, 1.0],
-#     [120/255, 120/255, 20/255, 1.0],
-
-#     # Purples (light → dark)
-#     [240/255, 200/255, 255/255, 1.0],
-#     [180/255, 120/255, 220/255, 1.0],
-#     [120/255, 60/255, 160/255, 1.0],
-#     [60/255, 20/255, 100/255, 1.0],
-# ]
+empty = 0
+road = 1
+house = 2
+shop = 3
 
 colors = [
     [0.9, 0.9, 0.9, 1.],
@@ -39,17 +18,28 @@ colors = [
     [120/255, 200/255, 120/255, 1.0],
     [120/255, 120/255, 200/255, 1.0],
 ]
-new_cmap = mcolors.ListedColormap(colors) 
 
-for _ in range(matrix_range):
-    line = []
+def setup() -> list:
+    matrix = []
+
     for _ in range(matrix_range):
-        line.append(0)
-    matrix.append(line)
+        line = []
+        for _ in range(matrix_range):
+            line.append(empty)
+        matrix.append(line)
 
-matrix[4][4] = 2
+    matrix[matrix_range//2][matrix_range//2] = house
+    matrix[matrix_range//2+1][matrix_range//2+1] = house
+    
+    return matrix
 
-def getNeighbor(i:int, j:int) -> list:
+fig, ax = plt.subplots(figsize=(7, 7))
+plt.tight_layout(pad=3.5)
+new_cmap = mcolors.ListedColormap(colors)
+cax = ax.imshow(setup(), cmap=new_cmap, interpolation="nearest", aspect='auto', vmin=0, vmax=len(colors))
+fig.colorbar(cax, fraction=0.03, pad=0.04)
+
+def getNeighbor(i:int, j:int, matrix) -> list:
     neighbor = []
     
     if i > 0: neighbor.append(matrix[i-1][j])
@@ -65,15 +55,13 @@ def getNeighbor(i:int, j:int) -> list:
     if i < matrix_range-1 and j < matrix_range-1: neighbor.append(matrix[i+1][j+1])
     return neighbor
 
-def chanceOfBuilding(i:int, j:int) -> int:
-    neighbor = getNeighbor(i, j)
-    prob = random.randint(0, 100)
-    if neighbor.count(2) == 0: return prob / 2
-
-    if neighbor.count(2) == 8: return prob * 3
-    elif neighbor.count(2) > 5: return prob * 2
-    elif neighbor.count(2) > 3: return prob * 1.5
-    else: return prob
+def chanceOfBuilding(i:int, j:int, matrix:list) -> int:
+    neighbor = getNeighbor(i, j, matrix)
+    prob = random.randint(0,500)
+    prob += (neighbor.count(house) * 1000)
+    if neighbor.count(house) > 3: prob += 1000
+    prob += (neighbor.count(shop) * 1500)
+    return prob
 
 def houseLevel(h:int, s:int):
     
@@ -84,29 +72,54 @@ def shopLevel(h:int, s:int):
     return
 
 def compute():
+    global matrix
+    global old_matrix
     for i in range(matrix_range):
         for j in range(matrix_range):
-            value = matrix[i][j]
-            prob = chanceOfBuilding(i, j)
-            if value == 2:
-                prob *= 2
-            if prob > 70: value = 2
-            else: value = 0
+
+            value = old_matrix[i][j]
+            prob = chanceOfBuilding(i, j, old_matrix)
+
+            if value != empty: prob += 1400
+
+            if prob < 1500: value = empty
+            elif prob > 2000:
+                shopNumber = getNeighbor(i, j, old_matrix).count(shop)
+                shopProb = 0
+                if shopNumber > 2:
+                    shopProb = 30
+                elif shopNumber < 6:
+                    shopProb = 10
+                elif shopNumber < 3:
+                    shopProb = 50
+                if value == shop: shopProb += 40
+                if random.randint(0,100) < shopProb: value = shop
+                else: value = house
+                if random.randint(0,100) < 30:
+                    value = empty
+            
             matrix[i][j] = value
+    old_matrix = copy.deepcopy(matrix)
 
 def roadGeneration():
     return
 
-def display():
-    cax = plt.imshow(matrix, cmap=new_cmap, interpolation="nearest", aspect='auto')
-    plt.xticks([])
-    plt.yticks([])
+def display(matrix):
+    cax = ax.imshow(matrix, cmap=new_cmap, interpolation="nearest", aspect='auto', vmin=0, vmax=len(colors))
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 def run(i):
+    global matrix
+    global old_matrix
+    if i == 0:
+        matrix = setup()
+        old_matrix = copy.deepcopy(matrix)
     compute()
-    display()
+    display(matrix)
 
 if __name__ == '__main__' :
-    ani = FuncAnimation(plt.gcf(), run, interval=1000)
+
+    ani = FuncAnimation(plt.gcf(), run, interval=2000)
     plt.tight_layout()
     plt.show()
