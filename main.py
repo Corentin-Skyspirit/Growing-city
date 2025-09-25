63e403cf9b476d164a709ae1fce1335084ee366b
Corentin Arnaud <corentin.arnaud45@gmail.com>
2025-09-25 16:46:17 +0200

    terrible diffusion

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 import copy
 
-matrix_range = 100
+matrix_range = 10
 
 empty = 0
 road = 1
@@ -39,7 +39,7 @@ new_cmap = mcolors.ListedColormap(colors)
 cax = ax.imshow(setup(), cmap=new_cmap, interpolation="nearest", aspect='auto', vmin=0, vmax=len(colors))
 fig.colorbar(cax, fraction=0.03, pad=0.04)
 
-def getNeighbor(i:int, j:int, matrix) -> list:
+def getNearNeighbor(i:int, j:int, matrix) -> list:
     neighbor = []
     
     if i > 0: neighbor.append(matrix[i-1][j])
@@ -53,15 +53,53 @@ def getNeighbor(i:int, j:int, matrix) -> list:
 
     if i < matrix_range-1 and j > 0: neighbor.append(matrix[i+1][j-1])
     if i < matrix_range-1 and j < matrix_range-1: neighbor.append(matrix[i+1][j+1])
+
     return neighbor
 
-def chanceOfBuilding(i:int, j:int, matrix:list) -> int:
-    neighbor = getNeighbor(i, j, matrix)
-    prob = random.randint(0,500)
-    prob += (neighbor.count(house) * 1000)
-    if neighbor.count(house) > 3: prob += 1000
-    prob += (neighbor.count(shop) * 1500)
+def getNeighbor(i:int, j:int, matrix) -> list:
+    neighbor = []
+
+    neighbor += matrix[i-2][j-1:j+2]
+    neighbor += matrix[i-1][j-2:j+3]
+    neighbor += matrix[i][j-2:j+3]
+    neighbor += matrix[i+1][j-2:j+3]
+    neighbor += matrix[i+2][j-1:j+2]
+
+    return neighbor
+
+def chanceOfBuilding(i:int, j:int, actualBuild: int, neighbor:list, nearNeighbor:list) -> int:
+    if actualBuild != empty: prob = 50 
+    else: prob = 0
+    
+    nearHouseCount = nearNeighbor.count(house)
+    nearShopCount = nearNeighbor.count(shop)
+    prob += nearHouseCount * 2
+    prob += nearShopCount * 4
+    prob += neighbor.count(house) - nearHouseCount
+    prob += (neighbor.count(shop) *2) - nearShopCount
+
     return prob
+
+def newBuildingType(i:int, j:int, actualBuild:int, neighbor:list, nearNeighbor:list):
+    buildingType = empty
+    houseChance = 0
+    shopChance = 0
+    if actualBuild == house: houseChance += 10
+    elif actualBuild == shop: shopChance += 10
+
+    houseChance += neighbor.count(shop)
+    houseChance += neighbor.count(empty)
+
+    nearShopCount = nearNeighbor.count(shop)
+    shopChance += neighbor.count(house)
+    shopChance += nearShopCount
+    shopChance -= neighbor.count(shop) - nearShopCount
+
+    if houseChance < shopChance:
+        buildingType = shop
+    else:
+        buildingType = house
+    return buildingType
 
 def houseLevel(h:int, s:int):
     
@@ -71,37 +109,37 @@ def shopLevel(h:int, s:int):
 
     return
 
+def newBuilding(i:int, j:int, matrix:list) -> int:
+    oldBuilding = matrix[i][j]
+    neighbor = getNeighbor(i, j, matrix)
+    if neighbor.count(empty) == 21 : return empty
+    else:
+        nearNeighbor = getNearNeighbor(i, j, matrix)
+        prob = chanceOfBuilding(i, j, oldBuilding, neighbor, nearNeighbor)
+        if prob > 100: print("oui")
+        building = newBuildingType(i, j, oldBuilding, neighbor, nearNeighbor)
+        if prob < 100:
+            if random.randint(0,100) < prob: building = empty
+        
+        print("[", i*matrix_range + j, "] builds: ", oldBuilding, " / ", building, "prob:", prob)
+
+    return building
+
 def compute():
     global matrix
     global old_matrix
-    for i in range(matrix_range):
-        for j in range(matrix_range):
+    for i in range(2, matrix_range-2):
+        for j in range(2, matrix_range-2):
 
-            value = old_matrix[i][j]
-            prob = chanceOfBuilding(i, j, old_matrix)
-
-            if value != empty: prob += 1400
-
-            if prob < 1500: value = empty
-            elif prob > 2000:
-                shopNumber = getNeighbor(i, j, old_matrix).count(shop)
-                shopProb = 0
-                if shopNumber > 2:
-                    shopProb = 30
-                elif shopNumber < 6:
-                    shopProb = 10
-                elif shopNumber < 3:
-                    shopProb = 50
-                if value == shop: shopProb += 40
-                if random.randint(0,100) < shopProb: value = shop
-                else: value = house
-                if random.randint(0,100) < 30:
-                    value = empty
+            value = newBuilding(i, j, old_matrix)
             
             matrix[i][j] = value
     old_matrix = copy.deepcopy(matrix)
 
 def roadGeneration():
+    return
+
+def riverGeneration():
     return
 
 def display(matrix):
